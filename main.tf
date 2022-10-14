d3ee0d267dd4a009c19d5a7b5f58096a75113d51
chrisland85 <99516920+chrisland85@users.noreply.github.com>
2022-10-14 14:17:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,71 +1,89 @@
 # Step 1 - Define the provider
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-# Data source for availability zones in us-east-1
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-locals {
-  default_tags = merge(
-    var.default_tags,
-    { "Env" = var.env }
-  )
-}
-
-# Create a new VPC 
-resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
-  tags = merge(
-    local.default_tags, {
-      Name = "${var.prefix}-public-subnet"
-    }
-  )
-}
-
-
-# Add provisioning of the public subnetin the default VPC
-resource "aws_subnet" "public_subnet" {
-  count             = length(var.private_cidr_blocks)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_cidr_blocks[count.index]
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  tags = merge(
-    local.default_tags, {
-      Name = "${var.prefix}-public-subnet-${count.index}"
-    }
-  )
-}
-
-# Create Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(local.default_tags,
-    {
-      "Name" = "${var.prefix}-igw"
-    }
-  )
-}
-
-# Route table to route add default gateway pointing to Internet Gateway (IGW)
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
   tags = {
-    Name = "${var.prefix}-route-public-route-table"
+    Name        = "${var.env}-vpc"
+    Env = var.env
   }
 }
 
-# Associate subnets with the custom route table
-resource "aws_route_table_association" "public_routes-table_association" {
-  count          = length(aws_subnet.public_subnet[*].id)
-  route_table_id = aws_route_table.public_route_table.id
-  subnet_id      = aws_subnet.public_subnet[count.index].id
+# Subnets
+# Internet Gateway for Public Subnet
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name        = "${var.env}-igw"
+    Env = var.env
+  }
+}
+
+# Elastic-IP (eip) for NAT
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ig]
+}
+
+# NAT
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
+
+  tags = {
+    Name        = "nat"
+    Env = "${var.env}"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  count                   = length(var.private_subnets_cidr)
+  cidr_block              = element(var.private_subnets_cidr, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.env}-${element(var.availability_zones, count.index)}-private-subnet"
+    Env = "${var.env}"
+  }
+}
+
+
+# Routing tables to route traffic for Private Subnet
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.env}-private-route-table"
+    Env = "${var.env}"
+  }
+}
+
+# Route for Internet Gateway
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ig.id
+}
+
+# Route for NAT
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets_cidr)
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id = aws_route_table.private.id
 }
