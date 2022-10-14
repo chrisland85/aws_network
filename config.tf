fcfb246a995b030162b6121ed197ba1188df0a7c
chrisland85 <99516920+chrisland85@users.noreply.github.com>
2022-10-14 14:32:10 -0400

    Update config.tf

--- config.tf
+++ config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "acs730-assignment1-ookusolubo"            // Bucket where to SAVE Terraform State
-    key    = "01-networking/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
+    bucket = "prod-assignment1-ookusolubo"            // Bucket where to SAVE Terraform State
+    key    = "prod-network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"                       // Region where bucket is created
   }
 }
